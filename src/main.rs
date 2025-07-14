17d4a1716d6138348532b15515a74539d26fe174
Timothy Spring <TimothyS@bayportfinance.com>
2025-07-14 20:56:30 +0200

    Call the function appropriately

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-use std::fs;
-
 use args::CliArgs;
 use clap::Parser;
 
 mod args;
 mod compressor;
 use anyhow::Result;
+use compressor::{compress_directory, compress_file};
 
 fn main() -> Result<()> {
     let args = CliArgs::parse();
 
-    if let Some(dir_name) = args.directory {
-        let dir_path = std::path::Path::new(&dir_name);
-        if dir_path.is_dir() {
-            //compress this guy
-        } else {
-            //throw an error
-        }
+    let path = std::path::Path::new(&args.path);
+    if !path.exists() {
+        panic!("Cannot compress non-existent path: {:?}", path)
     }
+    if path.is_dir() {
+        compress_directory(&path)?
+    } else {
+        compress_file(&path)?
+    };
 
     println!("I'mma I'mma Compressorizer, Compressorizer");
     Ok(())
