514fe4a33d2f94359bef3988207fad7f439d00fc
Timothy Spring <TimothyS@bayportfinance.com>
2025-07-14 20:56:02 +0200

    Rationalize
    Do not need both a file and dir arg

--- src/args.rs
+++ src/args.rs
@@ -7,8 +7,5 @@ use clap::Parser;
     long_about = "Flashcard to make knowledge stick like rust to metal"
 )]
 pub struct CliArgs {
-    #[arg(short, long, conflicts_with = "directory")]
-    pub files: Vec<String>,
-    #[arg(short, long, conflicts_with = "files")]
-    pub directory: Option<String>,
+    pub path: String,
 }
